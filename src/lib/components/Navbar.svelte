4501f8df5aaaafe4197a91888076d3f81755a4b1
ezra <ezra.furry@gmail.com>
2023-03-25 15:38:58 -0400

    updates

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,9 +1,12 @@
 <nav class="nav">
-	<div class="nav-header">The Paper Earth</div>
-	<hr />
+	<div class="nav-left">
+		<a class="nav-header" href="/">The Paper Earth</a>
+		<div class="nav-socials" />
+	</div>
 	<ul class="nav-links">
 		<li><a class="nav-link" href="/">Home</a></li>
 		<li><a class="nav-link" href="/about">About</a></li>
+		<li><a class="nav-link" href="/members">Members</a></li>
 		<li><a class="nav-link" href="/music">Music</a></li>
 		<li><a class="nav-link" href="/calendar">Calendar</a></li>
 		<li><a class="nav-link" href="/contact">Contact</a></li>
@@ -12,41 +15,39 @@
 
 <style lang="scss">
 	.nav {
+		@apply text-emerald-500;
 		padding: 16px;
-		height: 160px;
+		height: 64px;
 		display: flex;
-		flex-direction: column;
+		justify-content: space-between;
 		align-items: center;
-		background-color: black;
+	}
+
+	.nav-left {
+		display: flex;
+		align-items: center;
 	}
 
 	.nav-header {
 		font-family: 'Delicious Handrawn';
-		font-size: 48px;
-		margin-bottom: auto;
-	}
-
-	hr {
-		border: none;
-		height: 1px;
-		background-color: white;
-		width: 95%;
+		font-size: 40px;
+		margin-right: 16px;
 	}
 
 	.nav-links {
 		display: flex;
-		margin-top: 16px;
+		justify-content: center;
 
-		li:first-child .nav-link {
+		:first-child a {
 			margin-left: 0;
 		}
 
-		li:last-child .nav-link {
+		:last-child a {
 			margin-right: 0;
 		}
 	}
 
 	.nav-link {
-		margin: 0 24px;
+		margin: 0 20px;
 	}
 </style>
