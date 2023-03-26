e32153ca940241a3895a30f880bde51e3d625858
ezra <ezra.furry@gmail.com>
2023-03-26 01:42:12 -0400

    more shit!

--- src/lib/components/GoogleCalendar.svelte
+++ src/lib/components/GoogleCalendar.svelte
@@ -1,23 +1,16 @@
 <script async type="module" src="https://embed.styledcalendar.com/assets/parent-window.js"></script>
 
-<div class="calendar-parent">
-	<iframe
-		src="https://embed.styledcalendar.com/#LRkkpt41iG8Ebnx0SHZm"
-		title="Styled Calendar"
-		class="styled-calendar-container calendar"
-		data-cy="calendar-embed-iframe"
-	/>
-</div>
+<iframe
+	src="https://embed.styledcalendar.com/#LRkkpt41iG8Ebnx0SHZm"
+	title="Styled Calendar"
+	class="styled-calendar-container calendar"
+	data-cy="calendar-embed-iframe"
+/>
 
 <style lang="scss">
-	.calendar-parent {
-		height: 100vh;
-		overflow-y: auto;
-	}
-
 	.calendar {
-		max-width: 90vw;
 		height: 100%;
+		width: 100%;
 		border: none;
 	}
 </style>
--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -2,6 +2,6 @@
 	import GoogleCalendar from '$lib/components/GoogleCalendar.svelte';
 </script>
 
-<div class="flex flex-col items-center justify-center flex-auto p-4">
+<div class="flex flex-col items-center justify-center flex-auto p-0 md:p-8">
 	<GoogleCalendar />
 </div>
