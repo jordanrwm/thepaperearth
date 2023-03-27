674d804a4650e2a9a6e00321511ff924dd1f8f48
ezra <ezra.furry@gmail.com>
2023-03-26 20:09:59 -0400

    starting custom calendar

--- src/lib/components/GoogleCalendar.svelte
+++ src/lib/components/GoogleCalendar.svelte
@@ -1,16 +1,38 @@
-<script async type="module" src="https://embed.styledcalendar.com/assets/parent-window.js"></script>
+<script lang="ts" src="https://apis.google.com/js/platform.js">
+	const API_KEY = 'AIzaSyBLAH3R5fjawzfd6C4g78ffwNOi7E6oOGc';
+	const CLIENT_ID = '976224551943-32sbumm88pl8qhsu08oholtg0oll0bab.apps.googleusercontent.com';
+	const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+	const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
 
-<iframe
-	src="https://embed.styledcalendar.com/#LRkkpt41iG8Ebnx0SHZm"
-	title="Styled Calendar"
-	class="styled-calendar-container calendar"
-	data-cy="calendar-embed-iframe"
-/>
+	const start = async () => {
+		console.log('we started');
+		gapi.client
+			.init({
+				apiKey: API_KEY,
+				clientId: CLIENT_ID,
+				discoveryDocs: [DISCOVERY_DOC],
+				scope: SCOPES
+			})
+			.then(() => {
+				const request = {
+					calendarId: 'primary',
+					timeMin: new Date().toISOString(),
+					showDeleted: false,
+					singleEvents: true,
+					maxResults: 10,
+					orderBy: 'startTime'
+				};
+				console.log('hit', gapi.client);
+				// return gapi.client.request({
+				// 	path: 'https://www.googleapis.com/calendar/v3/users/me/calendarList'
+				// });
+			});
+	};
+
+	const initializeGapi = async () => {
+		gapi.load('client', start);
+	};
+</script>
 
 <style lang="scss">
-	.calendar {
-		height: 80vh;
-		width: 100%;
-		border: none;
-	}
 </style>
