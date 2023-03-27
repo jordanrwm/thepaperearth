598984d3654e5c04adbc52cf2e43f39a03fb97e0
jordan <jordanrwm@gmail.com>
2023-03-27 00:44:25 -0400

    I got this fucker working >:D

--- src/lib/components/GoogleCalendar.svelte
+++ src/lib/components/GoogleCalendar.svelte
@@ -1,38 +1,56 @@
-<script lang="ts" src="https://apis.google.com/js/platform.js">
+<script lang="ts">
 	const API_KEY = 'AIzaSyBLAH3R5fjawzfd6C4g78ffwNOi7E6oOGc';
 	const CLIENT_ID = '976224551943-32sbumm88pl8qhsu08oholtg0oll0bab.apps.googleusercontent.com';
 	const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
 	const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
 
-	const start = async () => {
-		console.log('we started');
-		gapi.client
-			.init({
-				apiKey: API_KEY,
-				clientId: CLIENT_ID,
-				discoveryDocs: [DISCOVERY_DOC],
-				scope: SCOPES
-			})
-			.then(() => {
-				const request = {
-					calendarId: 'primary',
-					timeMin: new Date().toISOString(),
-					showDeleted: false,
-					singleEvents: true,
-					maxResults: 10,
-					orderBy: 'startTime'
-				};
-				console.log('hit', gapi.client);
-				// return gapi.client.request({
-				// 	path: 'https://www.googleapis.com/calendar/v3/users/me/calendarList'
-				// });
-			});
+	let tokenClient: any;
+
+	const gapiLoaded = () => {
+		gapi.load('client', initializeGapiClient);
 	};
 
-	const initializeGapi = async () => {
-		gapi.load('client', start);
+	const gisLoaded = () => {
+		tokenClient = google.accounts.oauth2.initTokenClient({
+			client_id: CLIENT_ID,
+			scope: SCOPES,
+			callback: '' // defined later
+		});
 	};
+
+	const initializeGapiClient = async () => {
+		await gapi.client.init({
+			apiKey: API_KEY,
+			discoveryDocs: [DISCOVERY_DOC]
+		});
+	};
+
+	async function listUpcomingEvents() {
+		let response;
+		try {
+			const request = {
+				calendarId:
+					'cdf88ae954489f5da5453cf5c80d035f8d254ceeddc2e426800353ff091597ff@group.calendar.google.com',
+				timeMin: new Date().toISOString(),
+				showDeleted: false,
+				singleEvents: true,
+				maxResults: 10,
+				orderBy: 'startTime'
+			};
+			response = await gapi.client.calendar.events.list(request);
+			console.log('response: ', response);
+		} catch (err) {
+			return;
+		}
+	}
 </script>
+
+<svelte:head>
+	<script async defer src="https://apis.google.com/js/api.js" on:load={gapiLoaded}></script>
+	<script async defer src="https://accounts.google.com/gsi/client" on:load={gisLoaded}></script>
+</svelte:head>
+
+<button on:click={listUpcomingEvents}>Click Me!</button>
 
 <style lang="scss">
 </style>
