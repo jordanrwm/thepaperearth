6f5b493cf2bb382f14a82622f9f6a3b92a94d111
jordan <jordanrwm@gmail.com>
2023-03-27 02:05:42 -0400

    trying out a different calendar (?)

--- src/lib/components/GoogleCalendar.svelte
+++ src/lib/components/GoogleCalendar.svelte
@@ -1,16 +1,30 @@
-<script async type="module" src="https://embed.styledcalendar.com/assets/parent-window.js"></script>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { Calendar } from '@fullcalendar/core';
+	import dayGridPlugin from '@fullcalendar/daygrid';
+	import listPlugin from '@fullcalendar/list';
+	import googleCalendarPlugin from '@fullcalendar/google-calendar';
 
-<iframe
-	src="https://embed.styledcalendar.com/#LRkkpt41iG8Ebnx0SHZm"
-	title="Styled Calendar"
-	class="styled-calendar-container calendar"
-	data-cy="calendar-embed-iframe"
-/>
+	onMount(() => {
+		let calendarEl = document.getElementById('calendar') as HTMLElement;
+		let calendar = new Calendar(calendarEl, {
+			plugins: [googleCalendarPlugin, dayGridPlugin, listPlugin],
+			initialView: 'dayGridMonth',
+			height: 'auto',
+			googleCalendarApiKey: 'AIzaSyBLAH3R5fjawzfd6C4g78ffwNOi7E6oOGc',
+			events: {
+				googleCalendarId:
+					'cdf88ae954489f5da5453cf5c80d035f8d254ceeddc2e426800353ff091597ff@group.calendar.google.com'
+			}
+		});
+		calendar.render();
+	});
+</script>
+
+<div id="calendar" class="calendar" />
 
 <style lang="scss">
 	.calendar {
-		height: 80vh;
-		width: 100%;
 		border: none;
 	}
 </style>
