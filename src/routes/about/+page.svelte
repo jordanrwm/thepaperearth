0cb293b22075b39fe2ba8d6e4b78417cc8018bba
jordan <jordanrwm@gmail.com>
2023-03-28 20:24:28 -0400

    updated about page bio

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -13,12 +13,15 @@
 	<div class="basis-2/4 flex flex-col items-center justify-center md:h-3/4">
 		<h2 class="text-3xl md:text-5xl text-center mb-4">Who Are We?</h2>
 		<p class="px-4 md:px-16 text-lg md:text-2xl leading-10 text-center">
-			The Paper Earth is an American Alt-punk band that formed in Cincinnati, Ohio in 2018. With
-			their current lineup coming together in 2022. The band consists of vocalist/guitarist Sam
-			Moody, guitarist PJ Gebhart, guitarist CJ Havens, bassist Tommy Thornton, and drummer Zachary
-			Castonguay. The band's 2022 single "Blu" found them rocking the local music scene around
-			Cincinnati. You can find The Paper Earth in a local venue or house. The Paper Earth is set to
-			release their awaited debut album in 2023.
+			The Paper Earth is an American alt-punk band that formed in Cincinnati, Ohio in 2018, with the
+			current lineup coming together in 2022. The band consists of vocalist/guitarist Sam Moody,
+			guitarist PJ Gebhart, guitarist CJ Havens, bassist Tommy Thornton, and drummer Zachary
+			Castonguay. The Paper Earth calls on a variety of hardcore and alternative influences for the
+			single “Blu”, as well as the upcoming self-titled LP. Their high-energy live performances put
+			them on the map in Cincinnati Clifton music scene, frequenting stages such as Legends Bar &
+			Venue and The Mockbee, in addition to legendary house shows on UC campus alongside dozens of
+			the scenes best acts.The Paper Earths music is available on all streaming platforms and a
+			frequently updated calendar of live shows can be found on the home page.
 		</p>
 	</div>
 </div>
